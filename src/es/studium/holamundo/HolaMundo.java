fdb79cfba2b215676c5d9916021e6996ff27b09c
alvarobulnesgrupostudium <alvaro111612@gmail.com>
2023-12-01 08:40:10 +0100

    primercommit

---
+++ src/es/studium/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+package es.studium.holamundo;
+
+public class HolaMundo
+{
+
+	public static void main(String[] args)
+	{
+	System.out.println("Hola, mundo");	
+	System.out.println("que pasa bro!?");
+	System.out.println( "todo bien?");
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
